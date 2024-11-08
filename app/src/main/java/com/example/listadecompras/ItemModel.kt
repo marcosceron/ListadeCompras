0204d936690bf89d1ee610d509b1c1746eb4a0bf
Marcos <marcos.ceron89@gmail.com>
2024-11-08 14:26:49 -0300

    Initial commit

---
+++ app/src/main/java/com/example/listadecompras/ItemModel.kt
@@ -0,0 +1,3 @@
+package com.example.listadecompras
+
+data class ItemModel (val name: String, val onRemove:(ItemModel) -> Unit)
---
+++ app/src/main/java/com/example/listadecompras/ItemsAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.listadecompras
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemsAdapter: RecyclerView.Adapter<ItemsAdapter.ItemViewHolder>() {
+    class ItemViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val textView = view.findViewById<TextView>(R.id.textViewItem)
+        val button = view.findViewById<ImageButton>(R.id.imageButton)
+        fun bind(item: ItemModel) {
+            textView.text = item.name
+
+            button.setOnClickListener{
+                item.onRemove(item)
+            }
+        }
+    }
+
+    private val items = mutableListOf<ItemModel>()
+
+    fun addItem(newItem: ItemModel) {
+        items.add(newItem)
+        notifyDataSetChanged()
+    }
+
+    fun removeItem(item: ItemModel){
+        items.remove(item)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+
+        return ItemViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = items[position]
+        holder.bind(item)
+    }
+}
